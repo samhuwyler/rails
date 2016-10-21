d61819b47a52e5152dc0db1ae9041d5b75e359d0
Samuel Huwyler <shuwyler@EDUSOFT.local>
2016-10-21 12:17:59 +0200

    first commit

---
+++ TestApp/app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    
+    def hello
+        render html: "hello, world!"
+    end
+    
+    def goodby
+        render html: "goodby, world!"
+    end
+
+    
+end
